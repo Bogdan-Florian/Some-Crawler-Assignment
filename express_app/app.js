e89591ff8bffbb599e13f930d9b8f468c007dd47
Bogdan-Florian <aneab@coventry.ac.uk>
2023-07-11 22:05:56 +0100

    Added /start endpoint that takes as param how many websites to read from the csv. TODO: send the tasks to redis queue

--- express_app/app.js
+++ express_app/app.js
@@ -1,10 +1,31 @@
 import express from 'express'
+import fs from 'fs'
+import csv from 'csv-parser'
 
 export const app = express();
 const port = 3000;
 
 app.get('/start', (req, res) => {
-    res.send('process started');
+    let iterations = 0
+    const iterationsLimit = req.query.limit ? req.query.limit : 10;
+    console.log(iterationsLimit)
+    let stream = fs.createReadStream('sample-websites.csv').pipe(csv());
+
+    stream
+        .on('data', (row) => {
+            console.log(row)
+            if(iterations >= iterationsLimit){
+                stream.destroy(); // This stops the read stream
+            }
+            iterations += 1;
+        })
+        .on('end', () => {
+            console.log('CSV file successfully processed');
+        })
+        .on('close', () => {
+            console.log(`Finished reading only ${iterationsLimit}`)
+        });
+    res.send(`${iterationsLimit} websites have been sent for processing`)
 })
 
 
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 
-import { findAddressByTagStrategy, findNumberByTagStrategy, findNumberByKeywordsStrategy, findSocialMediaByTagStrategy } from './strategy/StrategyImplementations.js'
+// import { findAddressByTagStrategy, findNumberByTagStrategy, findNumberByKeywordsStrategy, findSocialMediaByTagStrategy } from './strategy/StrategyImplementations.js'
 
-const tags = [
-    '<p>Some text with a number: 123</p>',
-    '<span>Phone: +1 (123) 456-7890</span>',
-    '<div><p>Another paragraph with a phone number: 987654321</p></div>'
-  ];
+// const tags = [
+//     '<p>Some text with a number: 123</p>',
+//     '<span>Phone: +1 (123) 456-7890</span>',
+//     '<div><p>Another paragraph with a phone number: 987654321</p></div>'
+//   ];
 
-const findAddressByTagStrategyValues = findNumberByTagStrategy.executeStrategy(['<p>555555</p>', '<span>11111-11111</span>'], ['\\d+']);
-const findNumberByKeywordsValues = findNumberByKeywordsStrategy.executeStrategy(['<p>number:555555</p>', '<span>telephone 11111-11111</span>'], ['number', 'telephone']);
-console.log(findNumberByKeywordsValues)
+// const findAddressByTagStrategyValues = findNumberByTagStrategy.executeStrategy(['<p>555555</p>', '<span>11111-11111</span>'], ['\\d+']);
+// const findNumberByKeywordsValues = findNumberByKeywordsStrategy.executeStrategy(['<p>number:555555</p>', '<span>telephone 11111-11111</span>'], ['number', 'telephone']);
+// console.log(findNumberByKeywordsValues)
+
+import { app } from './express_app/app.js'
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+});
