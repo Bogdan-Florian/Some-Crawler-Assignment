5abd1f750d459a5d9fd9411b4b4bdff3a6edf0b6
Bogdan-Florian <aneab@coventry.ac.uk>
2023-07-11 22:53:44 +0100

    Sending each website to redis queue

--- express_app/app.js
+++ express_app/app.js
@@ -1,23 +1,23 @@
 import express from 'express'
 import fs from 'fs'
 import csv from 'csv-parser'
+import Queue from 'bull';
 
 export const app = express();
-const port = 3000;
+const websiteQueue = new Queue('websiteQueue', 'redis://127.0.0.1:6379');
 
 app.get('/start', (req, res) => {
-    let iterations = 0
-    const iterationsLimit = req.query.limit ? req.query.limit : 10;
-    console.log(iterationsLimit)
+    let iterations = 1
+    const iterationsLimit = req.query.limit ? Number(req.query.limit) : 10;
     let stream = fs.createReadStream('sample-websites.csv').pipe(csv());
 
-    stream
-        .on('data', (row) => {
-            console.log(row)
+        stream.on('data', (row) => {
             if(iterations >= iterationsLimit){
                 stream.destroy(); // This stops the read stream
             }
+            websiteQueue.add(row);
             iterations += 1;
+            
         })
         .on('end', () => {
             console.log('CSV file successfully processed');
@@ -29,3 +29,11 @@ app.get('/start', (req, res) => {
 })
 
 
+websiteQueue.process(async (job, done) => {
+    try {
+      console.log(`Website: ${job.data.domain}`);
+      done();
+    } catch (err) {
+      done(new Error(`Failed to retrieve website: ${err.message}`));
+    }
+  });
