a38b78033a7bdb0bb8e01d3be0884df76b7944f3
Bogdan-Florian <aneab@coventry.ac.uk>
2023-07-12 09:05:53 +0100

    Deleted StrategyManager. Although having another level of abstraction is nice to have, there's no need to delegate to an object all the parsing tasks in this specific probject. Also modified arrow functions to destructure the data received as it gives us more leeway in how we pass the data

--- strategy/StrategyImplementations.js
+++ strategy/StrategyImplementations.js
@@ -8,54 +8,85 @@ export const findAddressByTagStrategy = new Strategy('address', (...args) => fin
 export const findSocialMediaByTagStrategy = new Strategy('links', (...args) => findTargetByTag(...args));
 
 
-export function findTargetByTag(tags, regexExpTarget){
-  const results = [];
-  tags.forEach(tag => {
-    const matches = tag.match(regexExpTarget);
-    if (matches) {
-      results.push(matches[0]);
-    }
-  });
-  return results;
-}
+function findNumberByTags(details) {
+  const { cheerioElements, regexExpTarget } = details;
+  const maximumTagDataLength = 30;
+  const findUnwantedCharsRegex = '[^0-9 ]';
 
-
-
-export function extractTagsFromHTML(html, tags) {
-    const $ = cheerio.load(html);
-    const results = {};
-  
-    tags.forEach(tag => {
-      const elements = $(tag);
-      const extractedData = [];
-  
-      elements.each((index, element) => {
-        extractedData.push($(element).text());
-      });
-  
-      results[tag] = extractedData;
-    });
-  
-    return results;
-  }
-
-export function findNumberByKeywords(tags, keywords) {
-    const results = [];
-  
-    tags.forEach(tag => {
-      const value = tag.trim();
-      if (value.length < 50 && containsAnyKeyword(value, keywords)) {
-        const extractedNumbers = value.replace(/[^0-9]/g, '');
-        if (extractedNumbers) {
-          results.push(extractedNumbers);
+  let sanitisedPhoneNumbers = [];
+    cheerioElements('p, span').each((i, elem) => {
+      const data = cheerioElements(elem).text();
+      if(data.length < maximumTagDataLength){
+        const sanitizedData = sanitizeData(data, findUnwantedCharsRegex);
+        if (sanitizedData.length > 0) {
+          const phoneNumbers = splitIntoPhoneNumbers(sanitizedData);
+          sanitisedPhoneNumbers = [...sanitisedPhoneNumbers, ...phoneNumbers];
         }
       }
     });
-  
-    return results;
-  }
-  
-  function containsAnyKeyword(value, keywords) {
-    const lowercasedValue = value.toLowerCase();
-    return keywords.some(keyword => lowercasedValue.includes(keyword.toLowerCase()));
-  }
+    return sanitisedPhoneNumbers;
+}
+
+function sanitizeData(data, findUnwantedCharsRegex) {
+  const unwantedCharsRegexExp = new RegExp(findUnwantedCharsRegex, 'g');
+  return data.replace(unwantedCharsRegexExp, '');
+}
+
+function splitIntoPhoneNumbers(sanitizedData) {
+  const phoneNumbers = sanitizedData.split(' ');
+  return phoneNumbers.filter(number => number !== '');
+}
+
+
+function extractTagsFromHTML(details) {
+  const { html, tags } = details;
+  const $ = cheerio.load(html);
+  const results = {};
+
+  tags.forEach(tag => {
+    const elements = $(tag);
+    const extractedData = [];
+
+    elements.each((index, element) => {
+      extractedData.push($(element).text());
+    });
+
+    results[tag] = extractedData;
+  });
+
+  return results;
+}
+
+function findNumberByKeywords(details) {
+  const { tags, keywords } = details;
+  const results = [];
+
+  tags.forEach(tag => {
+    const value = tag.trim();
+    if (value.length < 50 && containsAnyKeyword(value, keywords)) {
+      const extractedNumbers = value.replace(/[^0-9]/g, '');
+      if (extractedNumbers) {
+        results.push(extractedNumbers);
+      }
+    }
+  });
+
+  return results;
+}
+
+export function findTagsWithHref($) {
+  const hrefTags = [];
+  $('*').each((index, element) => {
+    const tag = $(element).prop('tagName');
+    const href = $(element).attr('href');
+    if (href) {
+      hrefTags.push(tag);
+    }
+  });
+  return hrefTags;
+}
+
+export function containsAnyKeyword(value, keywords) {
+  const lowercasedValue = value.toLowerCase();
+  return keywords.some(keyword => lowercasedValue.includes(keyword.toLowerCase()));
+}
