c6177f5bf78178b4181f9a3f885623e5a48e5b42
Bogdan-Florian <aneab@coventry.ac.uk>
2023-07-12 09:09:07 +0100

    Implemented redis consumer that launches headless browser, searches for contact page and applies a phone number scraping strategy. Our best shot is to find everything on the contact page given the 1000websites/10 minutes

--- redis-consumer/consumer.js
+++ redis-consumer/consumer.js
@@ -1,34 +1,73 @@
+import puppeteer from 'puppeteer';
 import Queue from 'bull';
-import axios from 'axios';
+import { findNumberByTagStrategy } from '../strategy/StrategyImplementations.js';
+import cheerio from 'cheerio';
 
 // Define the job queue
 const websiteQueueConsumer = new Queue('websiteQueue', 'redis://127.0.0.1:6379');
 
 websiteQueueConsumer.process('website', async (job, done) => {
-    try {
-      console.log(`Fetching Website no.${job.data.rowNumber}: http://${job.data.website}`);
-      const response = await axios.get(`http://${job.data.website}`, { 
-      });
-      done();
-    } catch (err) {
-      done(new Error(`Failed to retrieve website: ${err.message}`));
-    }
-  });
+  try {
+    console.log(`Fetching Website no.${job.data.rowNumber}: http://${job.data.website}`);
 
-  websiteQueueConsumer.on('error', (error) => {
-    console.error(`Error in job queue: ${error.message}`);
+    const browser = await puppeteer.launch({
+        headless: 'new'
+    });
+    const page = await browser.newPage();
+    await page.goto(`http://${job.data.website}`, { waitUntil: 'networkidle0' }); // Wait until page is fully loaded
+
+    // Get all links on the page
+    const hrefs = await page.$$eval('a[href]', links => links.map(link => link.getAttribute('href')));
+
+    // Find a link that contains "/contact"
+    const contactLink = hrefs.find(href => href.includes('/contact'));
+
+    if (contactLink) {
+      console.log(`Contact page found: ${contactLink}`);
+      // Navigate to the contact page
+      let contactUrl;
+      // Check if link is relative or absolute
+      if (contactLink.startsWith('http')) {
+        contactUrl = contactLink;
+      } else {
+        contactUrl = new URL(contactLink, `http://${job.data.website}`).href;
+      }
+      await page.goto(contactUrl, { waitUntil: 'networkidle0' });
+      console.log(`Navigated to contact page: ${contactUrl}`);
+
+      // Extract the page's HTML content
+      const html = await page.content();
+      const $ = cheerio.load(html);
+
+      const phoneNumberPattern = `\\d+`;
+
+      const phoneNumbersByTag = findNumberByTagStrategy.executeStrategy({ cheerioElements: $, regexExpTarget: phoneNumberPattern });
+      
+      console.log(`Phone Numbers found by Tag Strategy: ${phoneNumbersByTag}`);
+    } else {
+      console.log('Contact page not found');
+    }
+
+    await browser.close();
+    done();
+  } catch (err) {
+    done(new Error(`Failed to retrieve website: ${err.message}`));
+  }
 });
 
+websiteQueueConsumer.on('error', (error) => {
+  console.error(`Error in job queue: ${error.message}`);
+});
 
 websiteQueueConsumer.on('completed', (job, result) => {
-console.log(`Job completed`);
+  console.log(`Job completed`);
 });
-  
+
 websiteQueueConsumer.on('failed', (job, err) => {
-console.log('Job failed', err);
-job.remove();
+  console.log('Job failed', err);
+  job.remove();
 });
 
 websiteQueueConsumer.on('stalled', (job) => {
-console.log(`Job ${job.id} is stalled`);
+  console.log(`Job ${job.id} is stalled`);
 });
